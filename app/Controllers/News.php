416c60b381bef0c555b79e8fef7e4d4b38c8227d
izzathabib <muhdizat.h@gmail.com>
2024-05-09 12:26:30 +0800

    Update show method in News controller

--- app/Controllers/News.php
+++ app/Controllers/News.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\NewsModel; // Declare model that we want to use
+use App\Models\NewsModel; 
+use CodeIgniter\Exceptions\PageNotFoundException;// Declare model that we want to use
 
 class News extends BaseController {
 
@@ -24,8 +25,21 @@ class News extends BaseController {
 
   // $slug is set to null to make it an optional.
   // The method does not necessarily being called with slug
+  // This method was used to display individual news item
   public function show(?string $slug=null) {
+    // Create $model as an instance to NewsModel class
     $model = model(NewsModel::class);
+
+    // Retrieve data and assign news as its variable name
     $data['news'] = $model->getNews($slug);
+
+    // Check if data is null
+    if ($data['news']==null) {
+      throw new PageNotFoundException('Cannot find the new item: '.$slug);
+    }
+
+    $data['title'] = $data['news']['title'];
+
+    return view('templates/header',$data).view('news/view').view('templates/footer');
   }
 }
