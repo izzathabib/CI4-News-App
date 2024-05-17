95524677767c36fe33519346d8d0ed934a5acbbf
izzathabib <muhdizat.h@gmail.com>
2024-05-17 14:45:57 +0800

    Remove "index.php" in site url

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,7 +1,17 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= esc($title) ?></title>
+
+    <base href="<?php echo base_url('assets') ?>/">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 <body>
     <!-- esc(): CodeIgniter global function
